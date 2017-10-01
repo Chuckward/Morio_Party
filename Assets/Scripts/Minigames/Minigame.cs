fe1f7d5c18756d88089b5c805f29222a375441b7
Chuck <andreas_ciachi@hotmail.com>
2017-10-01 15:22:39 +0200

    added HUD resource files and minigame class file

---
+++ Assets/Scripts/Minigames/Minigame.cs
@@ -0,0 +1,12 @@
+﻿
+public class MinigameListItem {
+
+    public string name;
+    public string sceneName;
+
+    public MinigameListItem(string name, string sceneName)
+    {
+        this.name = name;
+        this.sceneName = sceneName;
+    }
+}
