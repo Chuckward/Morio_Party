86599a8f68bc4a158ba0476afdd5ac35aac9608d
Chuck <andreas_ciachi@hotmail.com>
2017-10-02 02:44:11 +0200

    Added bowserspace scaffold, also script; Added ideas txt file for backup reasons

---
+++ Assets/Scripts/BowserSpace/BowserSpace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowserSpace : MonoBehaviour {
+
+    private MinigameListItem[] bowserGames = new MinigameListItem[] {
+    };
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
